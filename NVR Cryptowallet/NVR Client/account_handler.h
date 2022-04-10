19901f4e4ae5a2db4df02703fa5b88559f3dec60
Braun-Alex <72709229+Braun-Alex@users.noreply.github.com>
2022-04-10 14:04:20 +0300

    Update account_handler.h

--- NVR Cryptowallet/NVR Client/account_handler.h
+++ NVR Cryptowallet/NVR Client/account_handler.h
@@ -1,31 +1,40 @@
 #pragma once
+
+
+
 #include "cryptography/qaesencryption.h"
 #include "orm/inactivated_user.h"
 #include "orm/user.h"
 #include "orm/cryptowallet.h"
+
+
+
 class Account_Handler: public QObject
 {
 Q_OBJECT
+
+
+
 public:
 explicit Account_Handler(QObject* parent=nullptr);
-Q_INVOKABLE bool checkEmailCorrectness(const QString &email);   
+Q_INVOKABLE bool checkEmailCorrectness(const QString &email); 
 Q_INVOKABLE bool checkUserExisting(const QString &email);
 Q_INVOKABLE bool checkWalletExisting(const QString &wallet_address,
                                      const QString &email);
 Q_INVOKABLE bool signIn(const QString &email,
-                        const QString &password);
+                        const QString &password);                        
 Q_INVOKABLE void changePassword(const QString &email,
-                                const QString &new_password);
+                                const QString &new_password);                                
 Q_INVOKABLE bool recoverAccount(const QString &email);
 Q_INVOKABLE bool verifyRecoveringRequest(const QString &email);
 Q_INVOKABLE bool signUp(const QString &email,
-                        const QString &password);
+                        const QString &password);                        
 Q_INVOKABLE bool sendVerificationMessage(const QString &email,
-                                         const QString &verification_number);
+                                         const QString &verification_number);                                         
 Q_INVOKABLE bool sendRecoveringMessage(const QString &email,
-                                       const QString &verification_number);
+                                       const QString &verification_number);                                       
 Q_INVOKABLE bool sendNewPasswordMessage(const QString &email,
-                                        const QString &new_password);
+                                        const QString &new_password);    
 Q_INVOKABLE bool checkUserActivity(const QString &email);
 Q_INVOKABLE bool checkUserVerifyingStatus(const QString &email);
 Q_INVOKABLE bool checkVerificationNumber(const QString &email,
@@ -52,11 +61,16 @@ inline static QByteArray encrypt(const QString &text, const QString &passphrase,
                                  const QString &vector_phrase);
 inline static QByteArray decrypt(const QByteArray &text, const QString &passphrase,
                                  const QString &vector_phrase);
+                                 
+                                 
+                                 
 private:
-const size_t new_password_size=33, verification_number_size=99, seconds_in_day=86400;
+const size_t new_password_size=33, 
+             verification_number_size=99, 
+             seconds_in_day=86400;
 const QString cryptowallet_email="nvr.cryptowallet@gmail.com",
-cryptowallet_name="NVR Cryptowallet",
-cryptowallet_password="AlThanks123",
-vector_phrase="Cryptowallet";
+              cryptowallet_name="NVR Cryptowallet",
+              cryptowallet_password="AlThanks123",
+              vector_phrase="Cryptowallet";
 QRegularExpression email_validator;
 };
